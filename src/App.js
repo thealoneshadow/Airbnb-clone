840ebd02e220aca1b3cacad907f6a159637168f4
TheAloneShadow <43110655+thealoneshadow@users.noreply.github.com>
2021-01-20 12:22:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+          <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+
+      {/* Home */}
+
+      {/* Header */}
+
+      {/* Banner */}
+
+      {/* Search */}
+
+      {/* Card */}
+
+      {/* footer */}
+
+      {/* Search Page */}
+      {/* ..... */}
+    </div>
+  );
+}
+
+export default App;
